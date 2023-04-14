ec37f74ebc8a6f396693b14cc80c0c0d3e6d3965
wuzhanfei <wuzhanfei@baidu.com>
2023-04-14 06:46:24 +0000

    merge

--- symbolic_trace/trace.py
+++ symbolic_trace/trace.py
@@ -9,18 +9,6 @@ def symbolic_trace(func):
     def symbolic_traced_func(*args, **kw):
         ProxyTensorContext().reset()
         with SymbolicTraceContext() as ctx:
-<<<<<<< HEAD
-            paddle.fluid.core.set_eval_frame(eval_frame_callback)
-            returns = func(*args, **kw)
-            paddle.fluid.core.set_eval_frame(None)
-
-        # TODO( output analysis, we can get out symbols here. )
-        if returns is None:
-            return None
-
-        return SymbolicTraceContext().start_compile(ProxyTensorContext().get_runtime())
-    return wrapped
-=======
             with ConvertGuard(convert_function) as ctx:
                 paddle.fluid.core.set_eval_frame(eval_frame_callback)
                 try:
@@ -34,4 +22,3 @@ def symbolic_trace(func):
             output=returns)
         return ret
     return symbolic_traced_func
->>>>>>> 6b38ea48494f4f34e8ecc0f0f94dbcd7310ac0f5
